b8a02fac700ef4ed299442bf22dee062ff4115d0
uries242 <teykoa@gmail.com>
2026-05-14 18:00:38 -0400

    Completed Task4: Fetched from an External API

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT;
-
-
 
 app.get("/test", (req, res) => {
   res.send("Good Test!");
 });
 
-app.get("/api/fun-fact", (req, res) => {
-  res.send("Response");
+app.get("/api/fun-fact", async (req, res) => {
+  try {
+    const funFactApiResponse = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
+    res.json(funFactApiResponse.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch fun fact!" });
+  }
 });
-
-
-
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
-   
