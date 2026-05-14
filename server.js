a7575ba9c0d6639a7e16e1eccf7f134d42143427
uries242 <teykoa@gmail.com>
2026-05-14 18:49:50 -0400

    Completed Task5: Handle & Respond with Data, fixed issues causing bugs

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ app.get("/test", (req, res) => {
 app.get("/api/fun-fact", async (req, res) => {
   try {
     const funFactApiResponse = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
-    res.json(funFactApiResponse.data);
-  } catch (error) {
+    
+    const funFact = funFactApiResponse.data
+
+    const transformFunfactData = {
+        fact: funFact.text
+    };
+    
+    res.json(transformFunfactData);
+    
+} catch (error) {
     res.status(500).json({ error: "Failed to fetch fun fact!" });
-  }
+}
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
+
